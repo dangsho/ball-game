3b61dccbec0793b8dab16406908df2d427e0d5e8
Dangsho <mydangsho@gmail.com>
2024-12-05 05:34:44 +0330

    edit ball

--- ball.py
+++ ball.py
@@ -135,14 +135,16 @@ async def inline_query(update: Update, context):
         # ساختن متن پیام تاریخ و قیمت ثابت
         message = (
             f'@dangsho_bot\n'
-            f"⏰ تهران:\n{tehran_time.strftime('%H:%M:%S')}\n"
-            f"📅 تاریخ شمسی:\n{jalali_date.strftime('%Y/%m/%d')}\n"
-            f"📅 تاریخ میلادی:\n{gregorian_date}\n"
-            f"📅 تاریخ قمری:\n{hijri_date}\n"
+            
             f"💰 قیمت ارزهای دیجیتال:\n"
             f"₿ بیت‌کوین: ${bitcoin_price}\n"
             f" اتریوم: ${ethereum_price}\n"
             f"💵 تتر: {tether_price_toman} "
+            
+            f"⏰:\n{tehran_time.strftime('%H:%M:%S')}\n"
+            f"📅 تاریخ شمسی:\n{jalali_date.strftime('%Y/%m/%d')}\n"
+            f"📅 تاریخ میلادی:\n{gregorian_date}\n"
+            f"📅 تاریخ قمری:\n{hijri_date}\n"
         )
 
         # لینک بازی
