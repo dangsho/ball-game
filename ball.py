b14062068f1c0851ef9c24c4cee3c84e105ecae1
Dangsho <mydangsho@gmail.com>
2024-12-06 06:38:58 +0330

    edit ball

--- ball.py
+++ ball.py
@@ -48,11 +48,11 @@ def get_crypto_price_from_coinmarketcap(crypto_symbol):
             "X-CMC_PRO_API_KEY": "8baeefe8-4a9f-4947-8a9d-7f8ea40d91d3",
             "Accept": "application/json",
         }
-        params = {"symbol": crypto_symbol, "convert": "USD"}
+        params = {"symbol": crypto_symbol.upper, "convert": "USD"}
         response = requests.get(url, headers=headers, params=params)
         response.raise_for_status()
         data = response.json()
-        price = data["data"][crypto_symbol]["quote"]["USD"]["price"]
+        price = data["data"][crypto_symbol.upper]["quote"]["USD"]["price"]
         return f"{price:,.2f}"
     except requests.RequestException as e:
         logging.error(f"Error fetching data from CoinMarketCap: {e}")
